2ab5fa263d3f5e64b25aa515c1b916b56ab4514f
Victor Marchenko <viktor.marchenko@gmail.com>
2014-01-27 00:59:56 +0400

    Responsive optimization of design graphics

--- src/js/main.js
+++ src/js/main.js
@@ -27,54 +27,56 @@ document.addEventListener('DOMContentLoaded', function(){
 	dom.addEvent = function(element, eventName, callback){
 		element.addEventListener(eventName, callback);
 	};
-	var $navUp = dom.$('.js-navUp'),
-		$holder = dom.$('.js-holder'),
-		listHeight = parseInt(dom.getStyle(dom.$('.js-list')).height, 10),
-		$navDown = dom.$('.js-navDown'),
-		position = 0,
-		moveHolder = function(direction){
-			var current = parseInt(dom.getStyle($holder).top.replace('px', ''), 10);
-			$holder.style.top = current - listHeight*direction + 'px';
-			if (current - listHeight*direction === 0){
-				dom.addClass($navUp, 'm-inactive');
-			} else {
-				dom.removeClass($navUp, 'm-inactive');
-			}
-			if (Math.abs(current - listHeight*direction) + listHeight >= $holder.offsetHeight){
-				dom.addClass($navDown, 'm-inactive');
-			} else {
-				dom.removeClass($navDown, 'm-inactive');
-			}
-		};
-	dom.addEvent($navUp, 'click', function(){
-		if (!dom.hasClass($navUp,'m-inactive')){
-			moveHolder(-1);
-		}
-	});
-	dom.addEvent($navDown, 'click', function(e){
-		if (!dom.hasClass($navDown, 'm-inactive')){
-			moveHolder(1);
-		}
-	});
-	moveHolder(0);
-	var	$image = dom.$('.js-image'),
-		$preloader = dom.$('.js-preloader'),
-		$info = dom.$('.js-info'),
-		$elements = dom.$all('.js-galleryItem'),
-		element,
-		tmpImg = document.createElement('img'),
-		itemClickCallback = function(){
-			element = this;
-			dom.removeClass(dom.$('.js-galleryItem.m-active'), 'm-active');
-			dom.addClass(element, 'm-active');
-			tmpImg.onload = function(){
-				$image.setAttribute('src', tmpImg.getAttribute('src'));
-				$info.innerHTML = element.getAttribute('data-description');
-				tmpImg.onload = null;
+	if (dom.hasClass(document.body, 'p-gallery')){
+		var $navUp = dom.$('.js-navUp'),
+			$holder = dom.$('.js-holder'),
+			listHeight = parseInt(dom.getStyle(dom.$('.js-list')).height, 10),
+			$navDown = dom.$('.js-navDown'),
+			position = 0,
+			moveHolder = function(direction){
+				var current = parseInt(dom.getStyle($holder).top.replace('px', ''), 10);
+				$holder.style.top = current - listHeight*direction + 'px';
+				if (current - listHeight*direction === 0){
+					dom.addClass($navUp, 'm-inactive');
+				} else {
+					dom.removeClass($navUp, 'm-inactive');
+				}
+				if (Math.abs(current - listHeight*direction) + listHeight >= $holder.offsetHeight){
+					dom.addClass($navDown, 'm-inactive');
+				} else {
+					dom.removeClass($navDown, 'm-inactive');
+				}
 			};
-			tmpImg.setAttribute('src', element.getAttribute('data-big'));
-		};
-	for (var i = 0; i < $elements.length; i++) {
-		dom.addEvent($elements[i], 'click', itemClickCallback);
+		dom.addEvent($navUp, 'click', function(){
+			if (!dom.hasClass($navUp,'m-inactive')){
+				moveHolder(-1);
+			}
+		});
+		dom.addEvent($navDown, 'click', function(e){
+			if (!dom.hasClass($navDown, 'm-inactive')){
+				moveHolder(1);
+			}
+		});
+		moveHolder(0);
+		var	$image = dom.$('.js-image'),
+			$preloader = dom.$('.js-preloader'),
+			$info = dom.$('.js-info'),
+			$elements = dom.$all('.js-galleryItem'),
+			element,
+			tmpImg = document.createElement('img'),
+			itemClickCallback = function(){
+				element = this;
+				dom.removeClass(dom.$('.js-galleryItem.m-active'), 'm-active');
+				dom.addClass(element, 'm-active');
+				tmpImg.onload = function(){
+					$image.setAttribute('src', tmpImg.getAttribute('src'));
+					$info.innerHTML = element.getAttribute('data-description');
+					tmpImg.onload = null;
+				};
+				tmpImg.setAttribute('src', element.getAttribute('data-big'));
+			};
+		for (var i = 0; i < $elements.length; i++) {
+			dom.addEvent($elements[i], 'click', itemClickCallback);
+		}
 	}
 });
